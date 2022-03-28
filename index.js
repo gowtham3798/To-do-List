1b00be3bc1584a7d75880f364298f2617b6ef343
gowtham3798 <59119223+gowtham3798@users.noreply.github.com>
2022-03-28 13:37:06 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import * as React from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+
+
+
+
+
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+  <Router>
+    <App />
+  </Router>
+  
+   </StrictMode>
+  ,rootElement
+);
